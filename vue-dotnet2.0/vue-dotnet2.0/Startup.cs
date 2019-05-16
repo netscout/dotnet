e0040415c1652968724647b397ec9fc349ddd77b
netscout <brkang@mentoreet.com>
2019-05-16 14:45:02 +0900

    Initial commit

---
+++ vue-dotnet2.0/vue-dotnet2.0/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+
+namespace vue_dotnet2._0
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    //내용이 수정된 파일을 자동으로 교체하도록 설정
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                //사용자가 입력한 경로와 일치하는 컨트롤러 및 액션을 발견하지 못하면 Home/Index로 이동하도록 설정
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
---
+++ vue-dotnet2.0/vue-dotnet2.0/clientapp/main.js
@@ -0,0 +1,8 @@
+﻿import Vue from 'vue'
+
+var vm = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello Vue + Dotnet core 2.0"
+    }
+})
